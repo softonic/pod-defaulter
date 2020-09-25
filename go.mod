2ca260d95a6b1d25e916d236ab0b9dfe41b6643d
Riccardo Piccoli <riccardo.piccoli@gmail.com>
2020-09-25 11:41:22 +0200

    first draft

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/softonic/node-policy-webhook
+
+go 1.14
+
+require (
+	gotest.tools v2.2.0+incompatible
+	k8s.io/api v0.18.8
+	k8s.io/apimachinery v0.18.8
+	k8s.io/client-go v11.0.1-0.20190805182717-6502b5e7b1b5+incompatible
+	k8s.io/klog v1.0.0
+	knative.dev/pkg v0.0.0-20200925073843-7b99ff5a0bfa
+	sigs.k8s.io/controller-runtime v0.6.1
+)
---
+++ pkg/admission/reviewer.go
@@ -0,0 +1,109 @@
+package admission
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/softonic/node-policy-webhook/pkg/log"
+	"k8s.io/api/admission/v1beta1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog"
+	"knative.dev/pkg/apis/duck"
+)
+
+type AdmissionReviewer struct {
+	defaultTemplate *v1.PodTemplate
+}
+
+func NewPodDefaultValuesAdmissionReviewer(cm map[string]interface{}) *AdmissionReviewer {
+	return &AdmissionReviewer{
+		defaultTemplate: parseConfigIntoTemplate(cm),
+	}
+}
+
+func parseConfigIntoTemplate(cm map[string]interface{}) *v1.PodTemplate {
+	// @TODO: convert from map to PodTemplate
+	return &v1.PodTemplate{}
+}
+
+// PerformAdmissionReview : It generates the Adminission Review Response
+func (r *AdmissionReviewer) PerformAdmissionReview(admissionReview *v1beta1.AdmissionReview) {
+	pod, err := r.getPod(admissionReview)
+	if err != nil {
+		admissionReview.Response = r.newAdmissionError(pod, err)
+		return
+	}
+
+	defaultedPod := r.defaultPodValues(pod)
+
+	// if equals, don't patch
+	if equality.Semantic.DeepEqual(pod, defaultedPod) {
+		admissionReview.Response = r.admissionAllowedResponse(pod)
+		return
+	}
+
+	// If we encountered changes, then synthesize and apply
+	// a patch.
+	patchBytes, err := duck.CreateBytePatch(pod, defaultedPod)
+
+
+	if err != nil {
+		admissionReview.Response = r.newAdmissionError(pod, err)
+		return
+	}
+
+	klog.V(log.INFO).Infof("Patching pod %s/%s", pod.Namespace, pod.Name)
+	patchType := v1beta1.PatchTypeJSONPatch
+
+	admissionReview.Response = &v1beta1.AdmissionResponse{
+		Result: &v12.Status{
+			Status: "Success",
+		},
+		Patch:     patchBytes,
+		PatchType: &patchType,
+		Allowed:   true,
+		UID:       admissionReview.Request.UID,
+	}
+}
+
+func (r *AdmissionReviewer) newAdmissionError(pod *v1.Pod, err error) *v1beta1.AdmissionResponse {
+	if pod != nil {
+		klog.Errorf("Pod %s/%s failed admission review: %v", pod.Namespace, pod.Name, err)
+	} else {
+		klog.Errorf("Failed admission review: %v", err)
+	}
+	return &v1beta1.AdmissionResponse{
+		Result: &v12.Status{
+			Message: err.Error(),
+			Status:  "Fail",
+		},
+	}
+}
+
+func (r *AdmissionReviewer) admissionAllowedResponse(pod *v1.Pod) *v1beta1.AdmissionResponse {
+	klog.Errorf("Skipping admission review for pod %s/%s", pod.Namespace, pod.Name)
+	return &v1beta1.AdmissionResponse{
+		Allowed: true,
+	}
+}
+
+func (r *AdmissionReviewer) getPod(admissionReview *v1beta1.AdmissionReview) (*v1.Pod, error) {
+	var pod v1.Pod
+	if admissionReview.Request == nil {
+		return nil, errors.New("Request is nil")
+	}
+	if admissionReview.Request.Object.Raw == nil {
+		return nil, errors.New("Request object raw is nil")
+	}
+	err := json.Unmarshal(admissionReview.Request.Object.Raw, &pod)
+	if err != nil {
+		return nil, err
+	}
+	return &pod, nil
+}
+
+func (r *AdmissionReviewer) defaultPodValues(pod *v1.Pod) interface{} {
+	//@TODO: check properties of r.defaultTemplate, if not in pod, apply
+	return &v1.Pod{}
+}
